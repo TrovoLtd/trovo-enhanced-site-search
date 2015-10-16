5c2f1b9927e714108c22ff63aef11a22e5c94f4c
DaveGerrard <david.gerrard@trovo.co.uk>
2015-10-16 13:03:00 +0100

    Completed adding all the remaining properties to the IResult, so I can close Issue #8.

--- TESSCore/src/interfaces/interface.IResult.php
+++ TESSCore/src/interfaces/interface.IResult.php
@@ -5,6 +5,18 @@
 		public function getRank();
 		public function setRank($rank);
 		
+		public function getTitle();
+		public function setTitle($title);
+		
+		public function getUrl();
+		public function setUrl($url);
+		
+		public function getSnippet();
+		public function setSnippet($snippet);
+		
+		public function getFileType();
+		public function setFileType($fileType);
+		
 	}
 
 ?>
--- TESSCore/tests/test-google-site-search-module/test.google-result.php
+++ TESSCore/tests/test-google-site-search-module/test.google-result.php
@@ -13,7 +13,6 @@
 		
 		function setUp() {
 			$this->myGoogleResult = new GoogleResult();
-			
 		}
 		
 		function tearDown() {
@@ -25,6 +24,25 @@
 			$this->assertEquals(1, $this->myGoogleResult->getRank());
 		}
 		
+		function testTitle() {
+			$this->myGoogleResult->setTitle("Test title");
+			$this->assertEquals("Test title", $this->myGoogleResult->getTitle());
+		}
+		
+		function testUrl() {
+			$this->myGoogleResult->setUrl("Test URL");
+			$this->assertEquals("Test URL", $this->myGoogleResult->getUrl());
+		}
+		
+		function testSnippet() {
+			$this->myGoogleResult->setSnippet("Test Snippet");
+			$this->assertEquals("Test Snippet", $this->myGoogleResult->getSnippet());
+		}
+		
+		function testFileType() {
+			$this->myGoogleResult->setFileType("Test File Type");
+			$this->assertEquals("Test File Type", $this->myGoogleResult->getFileType());
+		}
 		
 	}
 	
